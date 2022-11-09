19a689c49ad3849171be0b1d981585f184c82028
n8hopp <112525308+n8hopp@users.noreply.github.com>
2022-11-09 15:35:43 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace DJFLAP
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.createNewDFAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.loadDFAFromFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.testDFAInputToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.testDFAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2,
+            this.toolStripButton5});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripDropDownButton1
+			// 
+			this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createNewDFAToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.loadDFAFromFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+			this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+			this.toolStripDropDownButton1.Text = "File";
+			this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+			// 
+			// createNewDFAToolStripMenuItem
+			// 
+			this.createNewDFAToolStripMenuItem.Name = "createNewDFAToolStripMenuItem";
+			this.createNewDFAToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.createNewDFAToolStripMenuItem.Text = "Create New DFA";
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.saveAsToolStripMenuItem.Text = "Save As";
+			// 
+			// loadDFAFromFileToolStripMenuItem
+			// 
+			this.loadDFAFromFileToolStripMenuItem.Name = "loadDFAFromFileToolStripMenuItem";
+			this.loadDFAFromFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.loadDFAFromFileToolStripMenuItem.Text = "Load DFA from file";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			// 
+			// toolStripDropDownButton2
+			// 
+			this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testDFAInputToolStripMenuItem,
+            this.testDFAToolStripMenuItem});
+			this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+			this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+			this.toolStripDropDownButton2.Size = new System.Drawing.Size(41, 22);
+			this.toolStripDropDownButton2.Text = "Run";
+			// 
+			// testDFAInputToolStripMenuItem
+			// 
+			this.testDFAInputToolStripMenuItem.Name = "testDFAInputToolStripMenuItem";
+			this.testDFAInputToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+			this.testDFAInputToolStripMenuItem.Text = "Test String (Input)";
+			// 
+			// testDFAToolStripMenuItem
+			// 
+			this.testDFAToolStripMenuItem.Name = "testDFAToolStripMenuItem";
+			this.testDFAToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+			this.testDFAToolStripMenuItem.Text = "Test String (Textfile)";
+			this.testDFAToolStripMenuItem.Click += new System.EventHandler(this.testDFAToolStripMenuItem_Click);
+			// 
+			// toolStripButton5
+			// 
+			this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+			this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton5.Name = "toolStripButton5";
+			this.toolStripButton5.Size = new System.Drawing.Size(36, 22);
+			this.toolStripButton5.Text = "Help";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.toolStrip1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+		private System.Windows.Forms.ToolStripMenuItem createNewDFAToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem loadDFAFromFileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+		private System.Windows.Forms.ToolStripMenuItem testDFAInputToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem testDFAToolStripMenuItem;
+		private System.Windows.Forms.ToolStripButton toolStripButton5;
+	}
+}
+
---
+++ State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJFLAP
+{
+	class State
+	{
+		private bool isSelected;
+		private string name;
+		private int id;
+		private Dictionary<int, char> transitions;
+		public State()
+		{
+			isSelected = false;
+			name = "";
+			id = -1;
+		}
+	}
+}
